162d7bfb4295cf7d547ba3c90c2a84ff25f60e86
CroitoriuAlexandru <75074772+CroitoriuAlexandru@users.noreply.github.com>
2023-02-11 19:29:40 +0200

    Update App.cpp

--- src/App.cpp
+++ src/App.cpp
@@ -1,9 +1,11 @@
 #include "include/App.h"
-
+#include <Wt/WPushButton.h>
 
 // Application is a containerWidget
 Application::Application()
 	: WContainerWidget()
 {
 	setStyleClass("m-auto mt-3");
+	auto testBtn = addWidget(std::make_unique<Wt::WPushButton>("i work"));
+	testBtn->setStyleClass("btn btn-success");
 }
